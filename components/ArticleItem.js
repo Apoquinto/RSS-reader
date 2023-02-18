58196bd41901bc0f298b5760053ec18201fd2340
Apoquinto <RCLlanesApo@gmail.com>
2023-02-18 01:38:23 -0600

    docs(Component): Add jsDoc to createArticleItem

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,3 +1,8 @@
+/**
+ * Creates an article element from RSS feed data
+ * @param {Element} articleData - The XML element representing the article from the RSS feed
+ * @returns {HTMLAnchorElement} - The HTML anchor element containing the article title and publication date
+ */
 const createArticleItem = ( articleData ) => {
   const item = document.createElement('a');
   const title = document.createElement('h3');
