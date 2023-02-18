f922676510d28c5df48c39e8a8eea522e4b3e8fa
Apoquinto <RCLlanesApo@gmail.com>
2023-02-17 22:42:38 -0600

    fix: remake util with camelCase and fix import

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import get_RSS_Content from "./utils/get_RSS_Content.js";
+import getRSSContent from "./utils/getRSSContent.js";
 
 const RSS_URL = 'https://tools.cdc.gov/podcasts/createrss.asp?t=r&c=151';
 
-get_RSS_Content(RSS_URL).then((xmlDoc) => {
+getRSSContent(RSS_URL).then((xmlDoc) => {
   const items = xmlDoc.querySelectorAll('item');
   const articles = document.getElementById('articles');
   items.forEach((item) => {
--- utils/getRSSContent.js
+++ utils/getRSSContent.js
@@ -3,10 +3,10 @@
  * @param {string} RSS_URL - The URL of the RSS feed.
  * @returns {Promise<Document>} - A promise that resolves to an XML document representing the contents of the RSS feed.
  */
-const get_RSS_Content = async (RSS_URL) => {
+const getRSSContent = async (RSS_URL) => {
   const response = await fetch(RSS_URL);
   const RSS_content = await response.text();
   return new window.DOMParser().parseFromString(RSS_content, "text/xml");
 }
 
-export default get_RSS_Content;
+export default getRSSContent;
